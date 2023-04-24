f05ed5bc27c6a47cb76641bf52925b5d4062f932
manishdodda <manish.dodda@pfizer.com>
2023-04-24 17:12:40 +0530

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh
 
 echo "DB_URL: $1"
 echo "DB_USERNAME: $2"
@@ -7,20 +7,22 @@ echo "DB_FAMILY: $4"
 echo "DB_CHANGELOGFILE: $5"
 echo "Dry_Run: $6"
 
-echo "Deploying objects into $4 Environment"
+echo "Deploying objects into $4 environment"
 
 if [ "$4" = "snowflake" ]; then
     driver="net.snowflake.client.jdbc.SnowflakeDriver"
 fi
 
-echo "Checking the Connection for $4 Environment"
+echo "Checking the connection for $4 environment"
 liquibase --url="$1" --username="$2" --password="$3" --driver="$driver" --changeLogFile="$5" status --verbose
 
-echo "Executing the Pre-check the SQL objects into $4 Env"
+echo "Executing the pre-check for SQL objects in $4 environment"
 liquibase --url="$1" --username="$2" --password="$3" --driver="$driver" --changeLogFile="$5" updateSQL
 
-echo "Deploying objects into $4 Environment"
+echo "Deploying objects into $4 environment"
 if [ "$6" = "No" ]; then
     echo "Executing update in $4 environment"
     liquibase --url="$1" --username="$2" --password="$3" --driver="$driver" --changeLogFile="$5" update
+else
+    echo "This is a Dry Run Deployment - Please check the GitHub Actions logs for deployments that might occur in the next run if you select the Dry Run option as No"
 fi
