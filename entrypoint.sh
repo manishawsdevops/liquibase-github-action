3c023ea082b85b0f84bfff840648912dd8f93fbf
manishdodda <manish.dodda@pfizer.com>
2023-04-24 14:53:41 +0530

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,22 @@ echo "DB_USERNAME: $2"
 echo "DB_PASSWORD: $3"
 echo "DB_FAMILY: $4"
 echo "DB_CHANGELOGFILE: $5"
-echo "Testing the Deployment into Snowflake"
+echo "Dry_Run: $6"
 
-ls
+echo "Deploying objects into $4 Environment"
 
-liquibase --url="$1" --username="$2" --password="$3" --driver="net.snowflake.client.jdbc.SnowflakeDriver" --changeLogFile="$5" status --verbose
+if [ $4 == "snowflake"]
+    driver = "net.snowflake.client.jdbc.SnowflakeDriver"
+fi
 
-# time=$(date)
-# echo "time=$time" >> $GITHUB_OUTPUT
+echo "Checking the Connection for $4 Environment"
+liquibase --url="$1" --username="$2" --password="$3" --driver="$driver" --changeLogFile="$5" status --verbose
+
+echo "Executing the Pre-check the SQL objects into $4 Env"
+liquibase --url="$1" --username="$2" --password="$3" --driver="$driver" --changeLogFile="$5" status --verbose
+
+echo "Deploying objects into $4 Environment"
+if [ $6 == "No"]
+    echo "Deploying the objects into $4 environment"
+    # liquibase --url="$1" --username="$2" --password="$3" --driver="$driver" --changeLogFile="$5" update
+fi
