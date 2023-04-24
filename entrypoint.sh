0159ff6ef7aff1a03f2d4d2d370ddf34aa59e4d7
manishdodda <manish.dodda@pfizer.com>
2023-04-24 16:23:15 +0530

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,7 @@ echo "Dry_Run: $6"
 
 echo "Deploying objects into $4 Environment"
 
-if [ $4 == "snowflake"]
-then
+if [ $4 == "snowflake"]; then
     driver = "net.snowflake.client.jdbc.SnowflakeDriver"
 fi
 
@@ -21,8 +20,7 @@ echo "Executing the Pre-check the SQL objects into $4 Env"
 liquibase --url="$1" --username="$2" --password="$3" --driver="$driver" --changeLogFile="$5" updateSQL
 
 echo "Deploying objects into $4 Environment"
-if [ $6 == "No"]
-then
+if [ $6 == "No"]; then
     echo "Deploying the objects into $4 environment"
     # liquibase --url="$1" --username="$2" --password="$3" --driver="$driver" --changeLogFile="$5" update
 fi
